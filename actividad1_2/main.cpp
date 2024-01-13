61719636c89466665ca83a12c5d0a78229ccef85
Fernando Israel Ríos García <93210601+IsraelRiverss@users.noreply.github.com>
2024-01-12 23:59:21 -0600

    Cambios en /actividad1_2/main.cpp, erro al leer testCases y imprimir vector

--- actividad1_2/main.cpp
+++ actividad1_2/main.cpp
@@ -30,13 +30,29 @@ using namespace std;
 
 int main ()
 {
-    //Vairables for the length of the array, the type of sorting algorithm, the type of search algorithm and the serched number in a sorted array
-    int length, typeSortAlgoritm, typeSearchAlgorithm, n; //Initialize variables
-    cin >> length; cin >> typeSortAlgoritm; cin >> typeSearchAlgorithm; cin >> n; //Delcare and read vairiables
+    //Vairables for the size of the array, the type of sorting algorithm, the type of search algorithm and the serched number in a sorted array
+    int size, typeSortAlgoritm, typeSearchAlgorithm, n; //Initialize variables
+    cin >> size; cin >> typeSortAlgoritm; cin >> typeSearchAlgorithm; cin >> n; //Delcare and read vairiables
     vector<int> vector; //Vector to store test cases
 
-    vector.resize(length); //Resize vector to length
-    for (int i = 0; i < length; i++) //Read test cases
-        
+    vector.resize(size); //Resize vector to size
+    for (int i = 0; i < size; i++) //Read test cases
+    {
+        cin >> vector[i];
+    }
+
+    //print vector
+    for (int i = 0; i < size; i++)
+    {
+        cout << vector[i] << " " << endl;
+    }    
+
+    //Instancia un objeto de la clase AlgoritmoSort, nombrado "sortObj"
+    AlgorithmSort<int> sortObj;
+    //Instancia un objeto de la clase SearchAlgorithm, nombrado "searchObj"
+    SearchAlgorithm<int> searchObj;
+
+    return 0;
     
+
 }
