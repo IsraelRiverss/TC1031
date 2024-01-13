e4951ab24e5611d42bc5bae10727cfd84670a8cc
Fernando Israel Ríos García <93210601+IsraelRiverss@users.noreply.github.com>
2024-01-12 20:56:55 -0600

    main con comentarios y ya se #include las clases Sort y Search algorithms.h

---
+++ actividad1_2/main.cpp
@@ -0,0 +1,33 @@
+// =========================================================
+// File: main.cpp
+// Author: Fernando Israel Rios Garcia-A01709118
+// Date: Noviembre 12th, 2024
+// =========================================================
+
+/*
+ Compilación con debug:
+ *  g++ -std=c++17 *.cpp -Wall -g -o main
+ *
+ * Compilación para ejecucion:
+ *  g++ -std=c++17 *.cpp -Wall -O3 -o main
+ *
+ * Ejecución con redireccion (input and output):
+ *  ./main < TestCases/t1.txt
+ *  ./main < TestCases/t2.txt > salida.txt
+*/
+
+//Files 
+#include "AlgorithmSort.h"
+#include "SearchAlgorithm.h"
+//Libraries
+#include <vector>
+#include <iostream>
+#include <chrono>
+
+
+using namespace std;
+
+int main ()
+{
+    
+}
