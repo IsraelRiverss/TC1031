9448be4048bd17055244a44fe5f4ed7edce357c2
Fernando Israel Ríos García <93210601+IsraelRiverss@users.noreply.github.com>
2024-01-13 00:45:40 -0600

    Cambios en /actividad1.2/main.cpp. Se agregaron dos switch cases para elegir los algoritmos de ordenamiento y busqueda. Tienen error al llamar los metdos de dichas claes de los algoritmos, falta poner adecuadamente los argumentos que requiere cada metodo para correr, el resto ya se puede leer los test caeses y guardar en el vector los datos de los test.tx. Esta comentado el codigo para impirimir el vector, borrar al final

--- actividad1_2/main.cpp
+++ actividad1_2/main.cpp
@@ -30,9 +30,9 @@ using namespace std;
 
 int main ()
 {
-    //Vairables for the size of the array, the type of sorting algorithm, the type of search algorithm and the serched number in a sorted array
-    int size, typeSortAlgoritm, typeSearchAlgorithm, n; //Initialize variables
-    cin >> size; cin >> typeSortAlgoritm; cin >> typeSearchAlgorithm; cin >> n; //Delcare and read vairiables
+    //Vairables for the size of the vector, the type of sorting algorithm, the type of search algorithm and the serched number in a sorted vector
+    int size, typeSortAlgorithm, typeSearchAlgorithm, numberToSearch; //Initialize variables
+    cin >> size; cin >> typeSortAlgorithm; cin >> typeSearchAlgorithm; cin >> numberToSearch; //Delcare and read vairiables
     vector<int> vector; //Vector to store test cases
 
     vector.resize(size); //Resize vector to size
@@ -40,19 +40,49 @@ int main ()
     {
         cin >> vector[i];
     }
+/* 
+NOTA DE FER:
+Puse este codigo para ver si el vector se leyo bien. si lo hace. Pero no se como mandar el vector a los metodos de ordenamiento y busqueda con los argumentos que piden para cada metedo
 
-    //print vector
+    //print vector. 
     for (int i = 0; i < size; i++)
     {
         cout << vector[i] << " " << endl;
     }    
-
-    //Instancia un objeto de la clase AlgoritmoSort, nombrado "sortObj"
+*/
+    // Instancia de los objetos para los algoritmos
     AlgorithmSort<int> sortObj;
-    //Instancia un objeto de la clase SearchAlgorithm, nombrado "searchObj"
     SearchAlgorithm<int> searchObj;
 
-    return 0;
-    
+    // Selección del algoritmo de ordenamiento
+    switch (typeSortAlgorithm) {
+        case 1: // Burbuja
+            sortObj.bubbleSort(vector); //Nota de Fer: No se que argumentos me faltan para que el metodo funcioe, ya defini las variables que nos que nospiden para leer el test case. 
+            break;
+        case 2: // Merge
+            sortObj.mergeSort(vector); //Nota de Fer: No se que argumentos me faltan para que el metodo funcioe, ya defini las variables que nos que nospiden para leer el test case. 
+            break;
+        case 3: // Quicksort
+            sortObj.quickSort(vector); //Nota de Fer: No se que argumentos me faltan para que el metodo funcioe, ya defini las variables que nos que nospiden para leer el test case. 
+            break;
+        default:
+            cerr << "Algoritmo de ordenamiento no válido" << endl;
+            return 1;
+    }
 
+    // Ejecución del algoritmo de búsqueda
+    int searchResult = -1;
+    switch (typeSearchAlgorithm) {
+        case 1: // Búsqueda Secuencial
+            searchResult = searchObj.busquedaSecuencial(vector, numberToSearch); //Nota de Fer: No se que argumentos me faltan para que el metodo funcioe, ya defini las variables que nos que nospiden para leer el test case. 
+            break;
+        case 2: // Búsqueda Binaria
+            searchResult = searchObj.busquedaBinaria(vector, numberToSearch); //Nota de Fer: No se que argumentos me faltan para que el metodo funcioe, ya defini las variables que nos que nospiden para leer el test case. 
+            break;
+        default:
+            cerr << "Algoritmo de búsqueda no válido" << endl;
+            return 1;
+    }
+
+    return 0;
 }
